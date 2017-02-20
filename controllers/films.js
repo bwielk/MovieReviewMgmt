8f94da1938b93abce2459508690d0b25e69c1473
Blazej <bwielk@gmail.com>
2017-02-20 22:25:44 +0000

    Basic RESTful done

--- controllers/films.js
+++ controllers/films.js
@@ -1,2 +1,29 @@
 var express = require('express');
 var filmsRouter = express.Router();
+
+var films = ["Trainspotting", "La Dolce Vita", "Birds", "The Hours", "Amelie"];
+
+filmsRouter.get('/', function(req, res){
+  res.json(films);
+});
+
+filmsRouter.get('/:id', function(req, res){
+  res.json({data: films[req.params.id]});
+});
+
+filmsRouter.post('/', function(req, res){
+  films.push(req.body.film);
+  res.json({data: films});
+});
+
+filmsRouter.put('/:id', function(req, res){
+  films[req.params.id] = req.body.film;
+  res.json({data: films});
+});
+
+filmsRouter.delete('/:id', function(req, res){
+  films.splice(req.params.id, 1);
+  res.json({data: films});
+});
+
+module.exports = filmsRouter;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/film', require('./film'));
 router.use('/films', require('./films'));
-
-router.get('/', function(req, res){
-  res.json({data: "Welcome"});
-});
 
 module.exports = router;
