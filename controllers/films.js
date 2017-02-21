1b6138fcde2ee1117ee4f8b692582b753e5e4cdb
Blazej <bwielk@gmail.com>
2017-02-21 10:10:51 +0000

    Completed

--- controllers/films.js
+++ controllers/films.js
@@ -13,12 +13,22 @@ filmsRouter.get('/:id', function(req, res){
 });
 
 filmsRouter.post('/', function(req, res){
-  films.push(req.body.film);
+  var film = new Film({
+    title: req.body.title,
+    actors:req.body.actors,
+    genre: req.body.genre
+  });
+  films.push(films);
   res.json({data: films});
 });
 
 filmsRouter.put('/:id', function(req, res){
-  films[req.params.id] = req.body.film;
+  var film = new Film({
+    title: req.body.title,
+    actors:req.body.actors,
+    genre: req.body.genre
+  });
+  films[req.params.id] = film;
   res.json({data: films});
 });
 
@@ -26,5 +36,18 @@ filmsRouter.delete('/:id', function(req, res){
   films.splice(req.params.id, 1);
   res.json({data: films});
 });
+
+//add review
+filmsRouter.post('/:id/reviews', function(req, rew){
+  var film = films[req.params.id];
+  review = new Review({
+    comment: req.body.comment,
+    rating: req.body.rate,
+    author: req.body.author
+  });
+  film.addReview(review);
+  res.json({data: films});
+
+})
 
 module.exports = filmsRouter;
